d02170a98cf57debe9b706f4a178d1596b5524e7
JavierCenteno <jacenve@telefonica.net>
2019-02-25 16:52:41 +0100

    Added show and list methods

--- D01/Acme-Madruga/src/main/java/controllers/AcmeFloatController.java
+++ D01/Acme-Madruga/src/main/java/controllers/AcmeFloatController.java
@@ -10,15 +10,21 @@
 
 package controllers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.AcmeFloatService;
+import services.BrotherhoodService;
 import domain.AcmeFloat;
+import domain.Brotherhood;
 
 @Controller
 @RequestMapping("/acmefloat")
@@ -28,12 +34,49 @@ public class AcmeFloatController extends AbstractController {
 
 	@Autowired
 	private AcmeFloatService	acmeFloatService;
+	@Autowired
+	private BrotherhoodService	brotherhoodService;
 
 
 	// Constructors -----------------------------------------------------------
 
 	public AcmeFloatController() {
 		super();
+	}
+
+	// Show -------------------------------------------------------------------
+
+	@RequestMapping(value = "/brotherhood/show", method = RequestMethod.GET)
+	public ModelAndView show(@RequestParam(value = "id") final int id) {
+		final ModelAndView result;
+		final AcmeFloat acmeFloat;
+		final Brotherhood brotherhood;
+
+		acmeFloat = this.acmeFloatService.findOne(id);
+		brotherhood = this.brotherhoodService.findPrincipal();
+		Assert.isTrue(acmeFloat.getBrotherhood().equals(brotherhood));
+
+		result = new ModelAndView("acmefloat/show");
+		result.addObject("acmeFloat", acmeFloat);
+
+		return result;
+	}
+
+	// List -------------------------------------------------------------------
+
+	@RequestMapping(value = "/brotherhood/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		final ModelAndView result;
+		final Collection<AcmeFloat> acmeFloats;
+		final Brotherhood brotherhood;
+
+		brotherhood = this.brotherhoodService.findPrincipal();
+		acmeFloats = brotherhood.getAcmeFloats();
+
+		result = new ModelAndView("acmefloat/list");
+		result.addObject("acmeFloats", acmeFloats);
+
+		return result;
 	}
 
 	// Create -----------------------------------------------------------------
@@ -68,5 +111,13 @@ public class AcmeFloatController extends AbstractController {
 
 		return result;
 	}
+
+	// Update -----------------------------------------------------------------
+
+	// TODO: update method
+
+	// Delete -----------------------------------------------------------------
+
+	// TODO: delete method
 
 }
