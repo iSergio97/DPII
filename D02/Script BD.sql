fb8d85cb538634d952ccab7bb940c095ffe4ddfc
Sergio <sergiotb15@gmail.com>
2019-03-11 16:07:16 +0100

    Script BD
    Creación script base de datos para el proyecto Acme-Parade

---
+++ D02/Script BD.sql
@@ -0,0 +1,27 @@
+#Borra la database si existe. En caso de no existir, no la borra.
+drop database if exists `Acme-Parade`;
+#drop database if exists `Acme-Madruga`;
+
+create database `Acme-Parade`;
+
+#No necesario si los usuarios no existen aun
+#Quitar en la parte de despliegue
+drop user 'acme-user';
+drop user 'acme-manager';
+
+
+create user 'acme-user'@'%'
+		identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
+
+#Clave que da para el Manager: e62c2bc3f155997006014fd3aaddeffc Clave cambiada
+create user 'acme-manager'@'%'
+		identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+grant select, insert, update, delete
+on `Acme-Parade`.* to 'acme-user'@'%';
+
+grant select, insert, update, delete, create, drop, references, index, alter,
+create temporary tables, lock tables, create view, create routine,
+alter routine, execute, trigger, show view
+on `Acme-Parade`.* to 'acme-manager'@'%';
+
