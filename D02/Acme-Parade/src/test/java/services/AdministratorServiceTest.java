7955a3d3f91014268367d1713f37187fa5709839
JavierCenteno <jacenve@telefonica.net>
2019-03-25 17:50:23 +0100

    Use test template

--- D02/Acme-Parade/src/test/java/services/AdministratorServiceTest.java
+++ D02/Acme-Parade/src/test/java/services/AdministratorServiceTest.java
@@ -32,76 +32,65 @@ public class AdministratorServiceTest extends AbstractTest {
 	@Autowired
 	private UserAccountRepository	userAccountRepository;
 
+	// Testing data -----------------------------------------------------------
+
+	private final Object[][]		testingData	= {
+		{
+		"AdministratorName", "AdministratorMiddleName", "AdministratorSurname", "https://www.imgur.com", "administrator_email@acme.com", "+123 (456) 7890", "AdministratorAddress", "AdministratorUsername", "AdministratorPassword", null
+		}, {
+		"AdministratorName", "AdministratorMiddleName", "AdministratorSurname", "IllegalPhoto", "IllegalEmail", "IllegalPhoneNumber", "AdministratorAddress", "AdministratorUsername", "AdministratorPassword", IllegalArgumentException.class
+		}
+												};
+
 
 	// Tests ------------------------------------------------------------------
 
 	/**
 	 * Attempts to save an administrator with the given data.
 	 */
-	private void saveAdministratorWith(final String username, final String password, final String name, final String middleName, final String surname, final String photo, final String email, final String phoneNumber, final String address) {
-		// Create administrator
-		Administrator administrator = this.administratorService.create();
-		UserAccount administratorUserAccount = administrator.getUserAccount();
-		administrator.setName(name);
-		administrator.setMiddleName(middleName);
-		administrator.setSurname(surname);
-		administrator.setPhoto(photo);
-		administrator.setEmail(email);
-		administrator.setPhoneNumber(phoneNumber);
-		administrator.setAddress(address);
-		administratorUserAccount.setUsername(username);
-		administratorUserAccount.setPassword(password);
-		// Save administrator
-		administratorUserAccount = this.userAccountRepository.save(administratorUserAccount);
-		administrator.setUserAccount(administratorUserAccount);
-		administrator = this.administratorService.save(administrator);
-		// Check if administrator has been saved correctly
-		Assert.isTrue(administrator.getName().equals(name));
-		Assert.isTrue(administrator.getMiddleName().equals(middleName));
-		Assert.isTrue(administrator.getSurname().equals(surname));
-		Assert.isTrue(administrator.getPhoto().equals(photo));
-		Assert.isTrue(administrator.getEmail().equals(email));
-		Assert.isTrue(administrator.getPhoneNumber().equals(phoneNumber));
-		Assert.isTrue(administrator.getAddress().equals(address));
-		Assert.isTrue(administrator.getUserAccount().isEnabled());
-		Assert.isTrue(administrator.getUserAccount().getUsername().equals(username));
-		Assert.isTrue(administrator.getUserAccount().getPassword().equals(password));
+	private void template(final String username, final String password, final String name, final String middleName, final String surname, final String photo, final String email, final String phoneNumber, final String address,
+		final Class<?> expectedThrowableClass) {
+		Class<?> throwableClass = null;
+		try {
+			// Create administrator
+			Administrator administrator = this.administratorService.create();
+			UserAccount administratorUserAccount = administrator.getUserAccount();
+			administrator.setName(name);
+			administrator.setMiddleName(middleName);
+			administrator.setSurname(surname);
+			administrator.setPhoto(photo);
+			administrator.setEmail(email);
+			administrator.setPhoneNumber(phoneNumber);
+			administrator.setAddress(address);
+			administratorUserAccount.setUsername(username);
+			administratorUserAccount.setPassword(password);
+			// Save administrator
+			administratorUserAccount = this.userAccountRepository.save(administratorUserAccount);
+			administrator.setUserAccount(administratorUserAccount);
+			administrator = this.administratorService.save(administrator);
+			// Check if administrator has been saved correctly
+			Assert.isTrue(administrator.getName().equals(name));
+			Assert.isTrue(administrator.getMiddleName().equals(middleName));
+			Assert.isTrue(administrator.getSurname().equals(surname));
+			Assert.isTrue(administrator.getPhoto().equals(photo));
+			Assert.isTrue(administrator.getEmail().equals(email));
+			Assert.isTrue(administrator.getPhoneNumber().equals(phoneNumber));
+			Assert.isTrue(administrator.getAddress().equals(address));
+			Assert.isTrue(administrator.getUserAccount().isEnabled());
+			Assert.isTrue(administrator.getUserAccount().getUsername().equals(username));
+			Assert.isTrue(administrator.getUserAccount().getPassword().equals(password));
+		} catch (final Throwable throwable) {
+			throwableClass = throwable.getClass();
+		}
+
+		super.checkExceptions(expectedThrowableClass, throwableClass);
 	}
 
-	/**
-	 * Attempts to save an administrator with legal data.
-	 */
 	@Test
-	public void saveGoodAdministratorTest() {
-		// Administrator fields
-		final String name = "AdministratorName";
-		final String middleName = "AdministratorMiddleName";
-		final String surname = "AdministratorSurname";
-		final String photo = "https://www.imgur.com";
-		final String email = "administrator_email@acme.com";
-		final String phoneNumber = "+123 (456) 7890";
-		final String address = "AdministratorAddress";
-		final String username = "AdministratorUsername";
-		final String password = "AdministratorPassword";
-		this.saveAdministratorWith(username, password, name, middleName, surname, photo, email, phoneNumber, address);
-	}
-
-	/*
-	 * Attempts to save an administrator with illegal data.
-	 */
-	@Test(expected = IllegalArgumentException.class)
-	public void saveBadAdministratorTest() {
-		// Administrator fields
-		final String name = "AdministratorName";
-		final String middleName = "AdministratorMiddleName";
-		final String surname = "AdministratorSurname";
-		final String photo = "IllegalPhoto";
-		final String email = "IllegalEmailAddress";
-		final String phoneNumber = "IllegalPhoneNumber";
-		final String address = "AdministratorAddress";
-		final String username = "AdministratorUsername";
-		final String password = "AdministratorPassword";
-		this.saveAdministratorWith(username, password, name, middleName, surname, photo, email, phoneNumber, address);
+	public void driver() {
+		for (int i = 0; i < this.testingData.length; ++i)
+			this.template((String) this.testingData[i][0], (String) this.testingData[i][1], (String) this.testingData[i][2], (String) this.testingData[i][3], (String) this.testingData[i][4], (String) this.testingData[i][5],
+				(String) this.testingData[i][6], (String) this.testingData[i][7], (String) this.testingData[i][8], (Class<?>) this.testingData[i][9]);
 	}
 
 }
