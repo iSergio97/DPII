4e57909cc84aa40991078bd89ec0510b88983b95
Sergio <sergiotb15@gmail.com>
2019-03-28 19:40:09 +0100

    Controlador y servicios hechos de inception record

--- D02/Acme-Parade/src/main/java/services/InceptionRecordService.java
+++ D02/Acme-Parade/src/main/java/services/InceptionRecordService.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 
 import repositories.InceptionRecordRepository;
 import security.Authority;
 import security.LoginService;
 import domain.InceptionRecord;
+import forms.InceptionRecordForm;
 
 @Service
 @Transactional
@@ -29,6 +32,9 @@ public class InceptionRecordService {
 
 	@Autowired
 	private BrotherhoodService			brotherhoodService;
+
+	@Autowired
+	private Validator					validator;
 
 
 	////////////////////////////////////////////////////////////////////////////////
@@ -101,5 +107,32 @@ public class InceptionRecordService {
 
 	public InceptionRecord getInceptionRecordByHistory(final int historyId) {
 		return this.inceptionRecordRepository.getInceptionRecordByHistory(historyId);
+	}
+
+	public InceptionRecordForm createForm() {
+		final InceptionRecordForm rf = new InceptionRecordForm();
+		rf.setDescription("");
+		rf.setPhotos(new ArrayList<String>());
+		rf.setTitle("");
+		return rf;
+	}
+
+	public InceptionRecord reconstruct(final InceptionRecordForm record, final BindingResult bindingResult) {
+		InceptionRecord result;
+
+		if (record.getId() == 0)
+			result = this.create();
+		else
+			result = this.inceptionRecordRepository.findOne(record.getId());
+
+		result.setTitle(record.getTitle());
+		result.setDescription(record.getDescription());
+		result.setPhotos(record.getPhotos());
+		result.setHistory(this.brotherhoodService.findPrincipal().getHistory());
+
+		this.validator.validate(result, bindingResult);
+		this.inceptionRecordRepository.flush();
+
+		return result;
 	}
 }
